0fd0f74edd2989824bfc2aac429b6488c3137d32
chetan21183 <chetanbarki63@gmail.com>
2025-04-23 12:13:10 +0530

    Save this in master

--- Employee.java
+++ Employee.java
@@ -2,13 +2,14 @@ public class Employee {
     private int id;
     private String name;
     private double salary;
+    private long phoneno;
 
     // Constructor
     public Employee(int id, String name, double salary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
-    }
+        this.phoneno = 0;    }
 
     // Getter methods
     public int getId() {
@@ -21,6 +22,10 @@ public class Employee {
 
     public double getSalary() {
         return salary;
+    }
+
+    public long getphoneno() {
+        return phoneno
     }
 
     // Setter methods
@@ -36,17 +41,22 @@ public class Employee {
         this.salary = salary;
     }
 
+    public void setphoneno(long phoneno) {
+        this.phoneno = phoneno;
+    }
+
     // Display employee details
     public void displayEmployeeDetails() {
         System.out.println("Employee ID: " + id);
         System.out.println("Employee Name: " + name);
         System.out.println("Employee Salary: " + salary);
+        System.out.println("Employee phone number:" + phoneno);
     }
 
     // Main method
     public static void main(String[] args) {
         // Creating an Employee object
-        Employee emp = new Employee(1, "John Doe", 50000.0);
+        Employee emp = new Employee(1, "John Doe", 50000.0 , 1234567890);
 
         // Displaying employee details
         emp.displayEmployeeDetails();
